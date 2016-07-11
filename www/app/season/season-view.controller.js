8af439aac4a55605dc12072daaacc3af3bad258d
Tim Michalski <tim@lssinc.com>
2016-07-11 01:00:49 -0500

    Updated Seasons & Events view to display data in more of a list/table view.

--- www/app/season/season-view.controller.js
+++ www/app/season/season-view.controller.js
@@ -13,6 +13,7 @@
     var vm = this;
 
     vm.season = getSeason();
+    vm.getFirstPlaceDiff = getFirstPlaceDiff;
     vm.viewEvent = viewEvent;
     vm.addEvent = addEvent;
     vm.seasonEdit = seasonEdit;
@@ -20,6 +21,12 @@
     function getSeason() {
       var id = $stateParams.id || 'latest';
       return seasonService.getSeason(id);
+    }
+
+    function getFirstPlaceDiff(amount) {
+      if (!amount) amount = 0;
+      var firstPlaceAmount = vm.season.rankings[0].winnings;
+      return amount - firstPlaceAmount;
     }
 
     function addEvent(params) {
